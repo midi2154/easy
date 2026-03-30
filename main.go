0e2842355af3d8ce9c80151cead7334a887304de
midi2154 <midi2154@gmail.com>
2026-03-30 02:47:44 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var a, b float64
+	var op string
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Simple CLI Calculator")
+	fmt.Println("Enter calculation (e.g. 5 + 3):")
+
+	// Read input
+	fmt.Fscan(reader, &a, &op, &b)
+
+	// Perform calculation
+	switch op {
+	case "+":
+		fmt.Println("Result:", a+b)
+	case "-":
+		fmt.Println("Result:", a-b)
+	case "*":
+		fmt.Println("Result:", a*b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Error: Division by zero")
+			return
+		}
+		fmt.Println("Result:", a/b)
+	default:
+		fmt.Println("Invalid operator. Use +, -, *, /")
+	}
+}
+
+///PALINDROMW
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(palindrom("no lemon no melon"))
+}
+
+func palindrom(word string) string {
+	reversed := ""
+	for i := len(word) - 1; i >= 0; i-- {
+		reversed += string(word[i])
+	}
+	return reversed
+}
+
+///METER TO KM AND BACK
+
+package main
+
+import "fmt"
+
+// km to meters
+func kmToM(km float64) float64 {
+	return km * 1000
+}
+
+// meters to km
+func mToKm(m float64) float64 {
+	return m / 1000
+}
+
+func main() {
+	fmt.Println("5 km =", kmToM(5), "meters")
+	fmt.Println("2000 m =", mToKm(2000), "km")
+}
+
+
+
